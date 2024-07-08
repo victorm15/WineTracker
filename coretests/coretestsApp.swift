a9fd4ee5aac186e2882bf0ae526a26571a5c9791
VictorM <vmauger449@gmail.com>
2024-07-08 20:26:32 +0200

    Initial Commit

---
+++ coretests/coretestsApp.swift
@@ -0,0 +1,20 @@
+//
+//  coretestsApp.swift
+//  coretests
+//
+//  Created by Victor Mauger on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct coretestsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
